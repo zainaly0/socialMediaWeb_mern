395fab5fe39b84d08be0cc0eec18a861478050c8
zainaly0 <zaidk4076@gmail.com>
2024-09-11 18:05:43 +0530

    backend REST API'S created and data is also store in database

--- server/models/User.js
+++ server/models/User.js
@@ -29,7 +29,7 @@ const UserSchema = new mongoose.Schema({
         defaul: ""
     },
     friends: {
-        type: String,
+        type: Array,
         default: []
     },
     location: String,
